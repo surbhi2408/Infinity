0a111c7a2051b77ef45b7c5f02c46f89d0cf6518
Surbhi Mayank <58289829+surbhi2408@users.noreply.github.com>
2020-12-23 19:11:26 +0530

    displaying comments

--- lib/screens/comments_page.dart
+++ lib/screens/comments_page.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social_network_app/screens/login_screen.dart';
 import 'package:social_network_app/widgets/header_widget.dart';
+import 'package:social_network_app/widgets/progress_widget.dart';
+import 'package:timeago/timeago.dart' as tAgo;
 
 class CommentsPage extends StatefulWidget {
 
@@ -22,6 +27,7 @@ class _CommentsPageState extends State<CommentsPage> {
   final String postId;
   final String postOwnerId;
   final String postImageUrl;
+  TextEditingController commentTextEditingController = TextEditingController();
 
   _CommentsPageState({
     this.postId,
@@ -30,8 +36,47 @@ class _CommentsPageState extends State<CommentsPage> {
   });
 
   // function where comments are displayed
-  displayComments(){
+  retrieveComments(){
+    return StreamBuilder(
+      stream: commentsReference.document(postId).collection("comments").orderBy("timestamp",descending: false).snapshots(),
+      builder: (context, dataSnapshot){
+        if(!dataSnapshot.hasData){
+          return circularProgress();
+        }
+        List<Comment> comments = [];
+        dataSnapshot.data.documents.forEach((document){
+          comments.add(Comment.fromDocument(document));
+        });
+        return ListView(
+          children: comments,
+        );
+      },
+    );
+  }
 
+  // after clicking to post, comment is saved to fireStore
+  saveComment(){
+    commentsReference.document(postId).collection("comments").add({
+      "username": currentUser.username,
+      "comment": commentTextEditingController.text,
+      "timestamp": DateTime.now(),
+      "url": currentUser.url,
+      "userId": currentUser.id,
+    });
+
+    bool isNotPostOwner = postOwnerId != currentUser.id;
+    if(isNotPostOwner){
+      activityFeedReference.document(postOwnerId).collection("feedItems").add({
+        "type": "comment",
+        "commentDate": timestamp,
+        "postId": postId,
+        "userId": currentUser.id,
+        "username": currentUser.username,
+        "userProfileImg": currentUser.url,
+        "url": postImageUrl,
+      });
+    }
+    commentTextEditingController.clear();
   }
 
   @override
@@ -41,7 +86,43 @@ class _CommentsPageState extends State<CommentsPage> {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: displayComments(),
+            child: retrieveComments(),
+          ),
+          Divider(),
+          ListTile(
+            title: TextFormField(
+              controller: commentTextEditingController,
+              decoration: InputDecoration(
+                labelText: "Write comment here...",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            trailing: OutlineButton(
+              onPressed: saveComment,
+              borderSide: BorderSide.none,
+              child: Text(
+                  "Post",
+                style: TextStyle(
+                  color: Colors.lightGreenAccent,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -50,9 +131,62 @@ class _CommentsPageState extends State<CommentsPage> {
 }
 
 class Comment extends StatelessWidget {
+
+  final String username;
+  final String userId;
+  final String url;
+  final String comment;
+  final Timestamp timestamp;
+
+  Comment({
+    this.username,
+    this.userId,
+    this.url,
+    this.comment,
+    this.timestamp,
+  });
+
+  factory Comment.fromDocument(DocumentSnapshot documentSnapshot){
+    return Comment(
+      username: documentSnapshot["username"],
+      userId: documentSnapshot["userId"],
+      url: documentSnapshot["url"],
+      comment: documentSnapshot["comment"],
+      timestamp: documentSnapshot["timestamp"],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.only(bottom: 6.0),
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                username + ":  " + comment,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
+              ),
+              leading: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(url),
+              ),
+              subtitle: Text(
+                tAgo.format(timestamp.toDate()),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Divider(height: 1.0,),
+          ],
+        ),
+      ),
+    );
   }
 }
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,6 +17,7 @@ final usersReference = Firestore.instance.collection("users");
 final StorageReference storageReference = FirebaseStorage.instance.ref().child("Posts Pictures");
 final postsReference = Firestore.instance.collection("posts");
 final activityFeedReference = Firestore.instance.collection("feed");
+final commentsReference = Firestore.instance.collection("comments");
 
 final DateTime timestamp = DateTime.now();
 User currentUser;
